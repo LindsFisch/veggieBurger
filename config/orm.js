27279026db0f35838c345b07e33ed874592b59a4
Lindsey Fischer <lindsfisch@gmail.com>
2017-07-24 20:59:10 -0500

    added files

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,15 @@
 //mySQL connection from connection.js
-var connection = require("connection.js");
+var connection = require("../config/connection.js");
 
 var methods = {
-    selectAll: function(){
-
+    selectAll: function(tableName, callBack){
+        var queryString = "SELET * FROM " + tableName + ";";
+        connection.query(queryString, function(err, res){
+            if (err) {
+                throw err;
+            }
+            callBack(res);
+        })
     },
     insertOne: function() {
 
