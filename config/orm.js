23b616350c9eb8c85c31c74272f1f3d07ea362c0
Lindsey Fischer <lindsfisch@gmail.com>
2017-07-26 21:13:51 -0500

    added formatting and routes

--- config/orm.js
+++ config/orm.js
@@ -1,23 +1,79 @@
 //mySQL connection from connection.js
 var connection = require("../config/connection.js");
 
-var methods = {
+
+//SQL Syntax helper
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++){
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+//SQL Syntax helper
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        if (Object.hasOwnProperty.call(ob, key)){
+            arr.push(key + "=" + ob[key]);
+        }
+    }
+
+    return arr.toString();
+}
+
+var orm = {
     selectAll: function(tableName, callBack){
-        var queryString = "SELET * FROM " + tableName + ";";
+        var queryString = "SELECT * FROM " + tableName + ";";
         connection.query(queryString, function(err, res){
             if (err) {
                 throw err;
             }
             callBack(res);
-        })
+        });
     },
-    insertOne: function() {
+    insertOne: function(tableName, col, vals, callBack) {
+        var queryString = "INSERT INTO " + tableName;
 
+        queryString += " (";
+        queryString += col.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, results){
+            if (err) {
+                throw err;
+            }
+            callBack(results);
+        });
     }, 
-    updateOne: function() {
+    updateOne: function(tableName, objColVals, condition, callBack){
+        var queryString = "UPDATE " + tableName;
+        
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
 
-    }
+        console.log(queryString);
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+
+            callBack(result);
+        });
+    } 
+    
 }
 
 //export ORM object
-module.exports = methods;
+module.exports = orm;
--- model/burger.js
+++ model/burger.js
@@ -1,20 +1,20 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: function (cb) {
+    selectAll: function(cb) {
         orm.selectAll("burgers", function(res){
-
-        })
+            cb(res);
+        });
     },
-    insertOne: function (cb) {
-        orm.insertOne(function(){
-
-        })
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res);
+        });
     },
-    updateOne: function(cb){
-        orm.updateOne(function(){
-
-        })
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(res){
+            cb(res);
+        });
     }
 }
 
